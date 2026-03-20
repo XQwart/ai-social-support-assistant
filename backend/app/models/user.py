542211532f337595e8473dbf45b4ae5d1f61693c
r0manb <romanse.b27@gmail.com>
2026-03-21 00:35:44 +0500

    Setup database

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from sqlalchemy import String, func
 from sqlalchemy.orm import Mapped, mapped_column
-from app.database import Base
+from app.core.database import Base
 
 
 class User(Base):
