32cc2ea4a484e1b310e6f775f9eb9c2fdbad9aa8
MonoBear123 <sakovic001@gmail.com~>
2026-03-22 19:44:43 +0500

    Add update chat

--- backend/app/services/chat.py
+++ backend/app/services/chat.py
@@ -20,21 +20,20 @@ class ChatService:
         self._chat_rep = chat_rep
 
     async def create_chat(self, user_id: int, message: str) -> Chat:
-
         title = message[:255]
 
         return await self._chat_rep.create(user_id=user_id, title=title)
 
     async def get_chats(self, user_id: int) -> tuple[list[Chat], int]:
-
         chats = self._chat_rep.get_all_by_user(user_id=user_id)
 
         return chats, len(chats)
 
     async def delete_chat(self, chat_id: int) -> bool:
-
         return self._chat_rep.delete(chat_id=chat_id)
 
     async def get_chat(self, chat_id: int) -> Chat:
-
         return self._chat_rep.get_by_id(chat_id=chat_id)
+
+    async def update_chat(self, chat_id: int, title: str | None = None) -> None:
+        self._chat_rep.update(chat_id=chat_id, title=title)
