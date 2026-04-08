017f0fe8cc81b24e4fa1ac436b41b1a6c9db5d4b
r0manb <romanse.b27@gmail.com>
2026-04-08 20:48:28 +0500

    Add api prefix

--- backend/app/routes/__init__.py
+++ backend/app/routes/__init__.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from . import auth_routes, chat_routes, message_routes, user_routes
 
 
-router = APIRouter()
+router = APIRouter(prefix="/api")
 router.include_router(auth_routes.router)
 router.include_router(chat_routes.router)
 router.include_router(message_routes.router)
